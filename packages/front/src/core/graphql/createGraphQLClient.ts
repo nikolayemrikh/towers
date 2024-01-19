d1378ce3b2c6e6cf30f89a109164a1d7350097ee
Nikolay Emrikh <nikolay.emrikh@stenn.com>
2024-01-19 11:26:15 +0100

    cleanup

--- packages/front/src/core/graphql/createGraphQLClient.ts
+++ packages/front/src/core/graphql/createGraphQLClient.ts
@@ -5,8 +5,6 @@ const url = 'http://localhost:54321/graphql/v1'
 
 export const createGraphQLClient = (): GraphQLClient => {
   return new GraphQLClient(url, {
-    // TODO: fix when this the type RequestExtendedInit will be exported https://github.com/jasonkuhrt/graphql-request/issues/421
-    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
     requestMiddleware: async (req) => {
       const token = (await supabase.auth.getSession()).data.session?.access_token;
 
