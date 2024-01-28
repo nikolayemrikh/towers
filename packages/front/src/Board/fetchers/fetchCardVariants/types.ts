aebd1aa1242687b38dd23124bcaf2a8e054d12fd
Nikolay Emrikh <nikolay.emrikh@stenn.com>
2024-01-28 11:52:18 +0100

    fix

--- packages/front/src/Board/fetchers/fetchCardVariants/types.ts
+++ packages/front/src/Board/fetchers/fetchCardVariants/types.ts
@@ -1,4 +1,4 @@
-import { Database } from '../../../../../shared/src/_supabase/database.types.ts';
+import { Database } from '@supabase/functions/_shared/database.types';
 
 export type TCardPower = Database['public']['Enums']['Power'];
 export type TCardVariants = Map<number, TCardPower>;
